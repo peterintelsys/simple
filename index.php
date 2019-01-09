a543aad86df3bc4eb2997ceb975dd6846647ce20
Peter <sppaulsson@gmail.com>
2019-01-09 12:54:18 +0100

    buttons

--- index.php
+++ index.php
@@ -35,8 +35,45 @@
 </div>
 
 <div class="container">
+
+<div class="grid-responsive grid-2-1">
+
+<div>	
+
+<h2 id="text">Text</h2>
+
 <h1>Välkommen</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
 <h2>Hej 123456789</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<hr>
+
+<h2 id="buttons">Buttons</h2>
+
+<p>
+	<button>Om oss</button>
+	<button class="button-primary">Om oss</button>
+	<button class="button btn-danger">Om oss</button>
+</p>
+<hr>
 
 <a href="javascript:void(0);" onclick="showid('one')">Testar hide</a>
 
@@ -44,7 +81,21 @@
 
 <div id="one" class="hide">Detta är gömd text</div>
 
-</div>	
+</div>
+
+
+<div>
+
+<h2>Innehåll</h2>
+
+<ul>
+	<li><a href="#text">Text</a></li>
+	<li><a href="#buttons">Buttons</a></li>
+</ul>
+
+</div>
+</div>
+
 <script type="text/javascript" src="assets/js/simple.js"></script>
 </body>
 
