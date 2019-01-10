4836f7d3a5b5c3ea176a7b34681bb37a859cfed4
Peter <sppaulsson@gmail.com>
2019-01-10 01:05:48 +0100

    homepage new

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
+    <title>Simple</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -28,15 +28,16 @@
 
 <h1 style="text-align: center;">Simple a CSS-framework</h1>
 
+<p style="display: flex; justify-content: space-around; "><a href="">Html-template</a> <a href="#css">CSS-file</a></p>
 
-
+<h4>Html-template</h4>
 <pre><code><xmp>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
+    <title>Simple</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -51,7 +52,7 @@
 
 <div class="container">
 	
-<div style="display: grid; justify-content: center; margin-top:50px;"><img style="width: 400%; height: auto;" src="assets/img/feather.svg">
+<div style="display: grid; justify-content: center; margin-top:50px;"><img src="assets/img/feather.svg">
 
 
 
@@ -67,7 +68,11 @@
 </html>
 </xmp></code></pre>
 
-
+<h4 id="css">CSS-file</h4><a href="">Tillbaka</a>
+<pre><code><xmp>
+<?php include('assets/css/simple.css')?>
+</xmp></code></pre>
+<a href="">Tillbaka</a>
 
 
 
